d28bafd7537f7f354001cbe0cb3e2bd60e5eadc5
Andrewmarac <marasigan.joshua.andrew@iisgalvanimi.edu.it>
2021-05-26 11:41:08 +0200

    aggiornamento 2.0

--- PythonSQL.py
+++ PythonSQL.py
@@ -25,31 +25,11 @@ cursore = conn.cursor()
 
 cont = 0
 for row in df.itertuples():
-    if row.Category == "Art":
-        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,'Art',%s,%s,%s)",
-        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Url),str(row.profile_image),str(row.description)))
+        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Category),str(row.Url),str(row.profile_image),str(row.description)))
         cont += 1
         print("1 record inserted")
-    if row.Category == "Sport":
-        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,'Sport',%s,%s,%s)",
-        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Url),str(row.profile_image),str(row.description)))
-        cont += 1
-        print("1 record inserted")
-    if row.Category == "Tech":
-        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,'Tech',%s,%s,%s)",
-        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Url),str(row.profile_image),str(row.description)))
-        cont += 1
-        print("1 record inserted")
-    if row.Category == "Food":
-        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,'Food',%s,%s,%s)",
-        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Url),str(row.profile_image),str(row.description)))
-        cont += 1
-        print("1 record inserted")
-    if row.Category == "Music":
-        cursore.execute("INSERT INTO Influencer(Screen_name, Name_, Number_of_Followers, Location_, Category_, Url_, Profile_image,Description) VALUES (%s,%s,%s,%s,'Music',%s,%s,%s)",
-        (str(row.Screen_name), str(row.Name),float(row.Number_of_Followers),str(row.Location),str(row.Url),str(row.profile_image),str(row.description)))
-        cont += 1
-        print("1 record inserted")
+  
 
 print(cont)
 conn.commit()
